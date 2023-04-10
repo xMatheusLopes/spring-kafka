6a42ac1af9533efc1c1194fe8a9fd095b12a24b0
Matheus Henrique Lopes <matheus.lopes@actdigital.com>
2023-04-10 15:13:20 -0300

    Timestamp seek test

--- src/main/java/com/matheus/springkafka/controllers/UserController.java
+++ src/main/java/com/matheus/springkafka/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.matheus.springkafka.controllers;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.matheus.springkafka.dtos.UserDTO;
 
 @RestController
-public class UserController {
+public class UserController implements ConsumerSeekAware {
 
     @Autowired
     private KafkaTemplate<String, UserDTO> kafkaTemplate;
@@ -25,14 +29,24 @@ public class UserController {
         return ResponseEntity.ok().body(kdto);
     }
 
-    @KafkaListener(id = "app-user", containerFactory = "UserDTOKafkaListenerContainerFactory",         
-        topicPartitions =
-        { 
-            @TopicPartition(topic = "user", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")),
-        }
-    )
+    // @KafkaListener(id = "app-user", containerFactory = "UserDTOKafkaListenerContainerFactory",         
+    //     topicPartitions =
+    //     { 
+    //         @TopicPartition(topic = "user", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")),
+    //     }
+    // )
+    // public void userDTOListener(ConsumerRecord<String, UserDTO> record) {
+    //     System.out.println("Received Message in dto: " + record.value().getName());
+    // }
 
-    public void userDTOListener(ConsumerRecord<String, UserDTO> record) {
-        System.out.println("Received Message in dto: " + record.value().getName());
+    @KafkaListener(id = "app-user", topics = "user", containerFactory = "UserDTOKafkaListenerContainerFactory")
+    public void listen(UserDTO user) {
+        System.out.println(user.getName());
+    }
+
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        long timestamp = System.currentTimeMillis()+60*1000;
+        System.out.println("Search for a time that is great or equal then {}" + timestamp);
+        callback.seekToTimestamp(new ArrayList<>(assignments.keySet()), timestamp);
     }
 }
